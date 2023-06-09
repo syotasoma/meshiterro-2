1c2c0986e0fb832181beac80abbc1a1f8b3f56f1
EC2 Default User <ec2-user@ip-172-31-35-22.ec2.internal>
2023-06-09 16:37:59 +0000

    2回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
   def edit
    @user = PostImage.find(params[:id])
   end
+  
+  def update
+    @user = PostImage.find(params[:id])
+    post_image.update(list_params)
+    redirect_to post_image(post_image.id)
+  end 
+ private
+ def user_params
+   params.require(:user).permit(:name, :profile_image)
+   
+ end
 end
